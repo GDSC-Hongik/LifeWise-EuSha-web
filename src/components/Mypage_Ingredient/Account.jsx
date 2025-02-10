a971e129c3d7deb5eeab9b46ab809db28c37caf8
kkangmen <pickofee@gmail.com>
2025-02-10 22:08:05 +0900

    Feat: 마이페이지 계정관리 카테고리 추가

--- src/components/Mypage_Ingredient/Account.jsx
+++ src/components/Mypage_Ingredient/Account.jsx
@@ -1,24 +1,42 @@
 import "./Account.css";
+import Profile from "../../assets/Image.png";
+import { useState } from "react";
 
 const Account = () => {
+  const [memberName, setmemberName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handlememberName = () => {
+    const newName = prompt("새로운 이름을 입력하세요: ", memberName);
+    setmemberName(newName);
+  };
+
+  const handlePassword = () => {
+    const newPassword = prompt("새로운 비밀번호를 입력하세요: ", password);
+    setPassword(newPassword);
+  };
+
   return (
     <div className="account-container">
-      <h2 className="mypageh2">계정 관리</h2>
+      <h1 className="account-title">내 계정 관리</h1>
 
       {/* 프로필 정보 */}
       <section className="profile-section">
-        <div className="profile-info">
-          <h3>김강민</h3>
-          <p>pickofee@gmail.com</p>
-        </div>
-        <button className="edit-btn">수정</button>
+        <h2>내 프로필</h2>
+        <img src={Profile} alt="프로필 사진"></img>
+        <h3>{memberName}</h3>
+        <p>서버로부터 받아온 이메일</p>
+        <button onClick={handlememberName} className="profile-btn">
+          이름 변경
+        </button>
       </section>
 
       {/* 비밀번호 변경 */}
       <section className="password-section">
-        <h3>비밀번호</h3>
-        <p>최근 업데이트:</p>
-        <button className="password-btn">비밀번호 변경</button>
+        <h2>비밀번호</h2>
+        <button onClick={handlePassword} className="password-btn">
+          비밀번호 변경
+        </button>
       </section>
     </div>
   );
