e1f1ff143f2045dfac2e54e3d0d3e2a288fd43ce
kkangmen <pickofee@gmail.com>
2025-02-18 23:15:54 +0900

    Feat: 조건문 수정

--- src/components/Mypage_Ingredient/Bookmarklist.jsx
+++ src/components/Mypage_Ingredient/Bookmarklist.jsx
@@ -16,21 +16,23 @@ const Bookmarklist = () => {
       <h1>내 북마크 목록</h1>
 
       <ul className="likes-list">
-        {bookMarks.length > 0
-          ? bookMarks.map((bookmark) => (
-              <li key={bookmark.bookmarkId} className="like-item">
-                <img
-                  src={bookmark.detail.imageUrl}
-                  alt={bookmark.detail.title}
-                  className="like-image"
-                />
-                <div className="like-info">
-                  <h3>{bookmark.detail.title}</h3>
-                  <p>{bookmark.detail.description}</p>
-                </div>
-              </li>
-            ))
-          : !(<p>북마크한 항목이 없습니다.</p>)}
+        {bookMarks.length > 0 ? (
+          bookMarks.map((bookmark) => (
+            <li key={bookmark.bookmarkId} className="like-item">
+              <img
+                src={bookmark.detail.imageUrl}
+                alt={bookmark.detail.title}
+                className="like-image"
+              />
+              <div className="like-info">
+                <h3>{bookmark.detail.title}</h3>
+                <p>{bookmark.detail.description}</p>
+              </div>
+            </li>
+          ))
+        ) : (
+          <p>북마크한 항목이 없습니다.</p>
+        )}
       </ul>
     </div>
   );
